c6d98db890b516dae30b6cb2cc60109e38342335
Tom Schmidt <z0phim@yahoo.com>
2015-07-07 12:50:57 -0700

    Added jQuery and Bootstrap and fixed remove functionality.

--- todo/urls.py
+++ todo/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^create/$', views.create, name='create'),
-    url(r'^remove$', views.remove, name='remove'),
+    url(r'^remove/(?P<id>[0-9]+)/$', views.remove, name='remove'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, HttpResponseBadRequest
 from django.views.decorators.http import require_POST, require_http_methods
 from .models import Todo
@@ -23,15 +23,17 @@ def create(request):
     name = request.POST.get('name')
     if not name:
         return HttpResponseBadRequest("Must provide a name.")
-    last = Todo.objects.order_by('-order')[:1].get()
-    todo = Todo.objects.create(name=name, order=last.order + 1, user=request.user)
-    return HttpResponse("Okay")
+    latest_order = 0
+    if Todo.objects.count() > 0:
+        latest_order = Todo.objects.order_by('-order')[:1].get().order
+    todo = Todo.objects.create(name=name, order=latest_order + 1, user=request.user)
+    return redirect('todo.views.index')
 
 @require_http_methods(['DELETE'])
 def remove(request, id):
     if not request.user.is_authenticated():
         return HttpResponseBadRequest("Must be signed in to remove a Todo item.")
-    todo = Todo.objects.get_object_or_404(id)
+    todo = get_object_or_404(Todo, pk=id)
     if request.user != todo.user:
         return HttpResponseBadRequest("Must own the Todo item to remove it.")
     todo.delete()
